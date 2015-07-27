c53319ce478313a67e22b3457006108a961fae9b
Nat Budin <nbudin@patientslikeme.com>
2015-07-27 14:12:30 -0400

    Multiple complexity thresholds (I think) matching Code Climate

--- Support/flog.rb
+++ Support/flog.rb
@@ -7,7 +7,10 @@ rescue LoadError
   exit 1
 end
 
-COMPLEXITY_WARNING_THRESHOLD = 25
+COMPLEXITY_THRESHOLDS = {
+  note: 25,
+  warning: 50
+}
 
 if ARGV.include? '-d'
   require 'flog_cli'
@@ -19,22 +22,24 @@ else
     flog = Flog.new(quiet: true, continue: true, all: true)
     flog.flog ENV['TM_FILEPATH']
     
-    system ENV['TM_MATE'], "--clear-mark=warning"
+    COMPLEXITY_THRESHOLDS.keys.each do |level|
+      system ENV['TM_MATE'], "--clear-mark=#{level}"
+    end
 
     flog.each_by_score do |class_method, score, call_list|
-      next unless score >= COMPLEXITY_WARNING_THRESHOLD
+      level, _ = COMPLEXITY_THRESHOLDS.select { |_, threshold| threshold <= score }.max_by { |_, threshold| threshold }
+      next unless level
       
       location = flog.method_locations[class_method]
       line = location && location.gsub(/.*:/, '')
 
       if line
-        message = "Method #{class_method} complexity: %0.2f" % [score]
-        system ENV['TM_MATE'], "--line=#{line}", "--clear-mark=warning", "--set-mark=warning:#{message}"
+        message = "#{class_method} complexity: %0.2f" % [score]
+        system ENV['TM_MATE'], "--line=#{line}", "--set-mark=#{level}:#{message}"
       end
     end
   rescue Racc::ParseError
   rescue Exception => e
-    puts e.class.name
-    puts e.message
+    puts "Error while running Flog:\n#{e.class.name} - #{e.message}"
   end
 end
