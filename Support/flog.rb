92be884394ca85118f824d3d35897fa6fea99cb9
Nat Budin <nbudin@patientslikeme.com>
2015-07-27 10:05:05 -0400

    Don't break if there's a parser error

--- Support/flog.rb
+++ Support/flog.rb
@@ -13,16 +13,22 @@ if ARGV.include? '-d'
   flog_cli.flog [ARGV[0], '-d', ENV['TM_FILEPATH']]
   flog_cli.report
 else
-  flog = Flog.new
-  flog.flog ENV['TM_FILEPATH']
+  begin
+    flog = Flog.new(quiet: true, continue: true)
+    flog.flog ENV['TM_FILEPATH']
 
-  flog.each_by_score Flog::THRESHOLD do |class_method, score, call_list|
-    location = flog.method_locations[class_method]
-    line = location && location.gsub(/.*:/, '')
+    flog.each_by_score Flog::THRESHOLD do |class_method, score, call_list|
+      location = flog.method_locations[class_method]
+      line = location && location.gsub(/.*:/, '')
   
-    if line
-      message = "Flog score: %0.2f" % [score]
-      system ENV['TM_MATE'], "--line=#{line}", "--clear-mark=warning", "--set-mark=warning:#{message}"
+      if line
+        message = "Flog score: %0.2f" % [score]
+        system ENV['TM_MATE'], "--line=#{line}", "--clear-mark=warning", "--set-mark=warning:#{message}"
+      end
     end
+  rescue Racc::ParseError
+  rescue Exception => e
+    puts e.class.name
+    puts e.message
   end
 end
